6ae1ab33994763e26a5699d2a8af363797a17e0a
JM012 <60477417+JM012@users.noreply.github.com>
2022-05-30 13:26:00 +0200

    Update getWeb3.js

--- src/getWeb3.js
+++ src/getWeb3.js
@@ -8,11 +8,12 @@ let HDWalletProvider = require("truffle-hdwallet-provider");
 const getWeb3 = () =>
   new Promise((resolve, reject) => {
 
-        const provider = new HDWalletProvider(
+        const provider = new HDWalletProvider(//Creates the wallet connection using the private key of a valid wallet to sign the transactions to call the smart contract's functions
+                                              //and the key of the gateway API which connects us to blockchain we want to use 
           process.env.PRIVATE_KEY, 
           process.env.INFURA_API
         );
-        const web3 = new Web3(provider);
+        const web3 = new Web3(provider);//using the provider we connect to Web3, this allow the code to interact with the blockchain
         resolve(web3);  
   });
 
